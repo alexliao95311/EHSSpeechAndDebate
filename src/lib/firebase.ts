ecf4e6637979e8b9e1792d716c3880c88502d39b
alex l <alexliao365@gmail.com>
2026-08-16 12:35:29 -0700

    firebase udpate

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase config object from Firebase console
 const firebaseConfig = {
-  apiKey: "AIzaSyAhL9SlURRY8-GWAIpUw-OG5d67IA4NHlc",
-  authDomain: "high-school-club-websites.firebaseapp.com",
-  projectId: "high-school-club-websites",
-  storageBucket: "high-school-club-websites.appspot.com",
-  messagingSenderId: "168079573394",
-  appId: "1:168079573394:web:4095e2067ce624301c014b",
-  measurementId: "G-C983T9TLYQ"
+  apiKey: "AIzaSyDla9e3tLkauAEnnAhS69GxHkdMJRXDVeU",
+  authDomain: "ehsdebatewebsite.firebaseapp.com",
+  projectId: "ehsdebatewebsite",
+  storageBucket: "ehsdebatewebsite.firebasestorage.app",
+  messagingSenderId: "195763757352",
+  appId: "1:195763757352:web:1958ecd1838390887b301d",
+  measurementId: "G-J8DJRJLNT2"
 };
 
 
